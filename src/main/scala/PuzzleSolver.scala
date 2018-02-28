d3a5003cdacf86087b303c6409f6829c9ef32afe
rahull33t <rahulkumar.goyal@utdallas.edu>
2018-02-28 13:15:17 -0600

    Check

--- src/main/scala/PuzzleSolver.scala
+++ src/main/scala/PuzzleSolver.scala
@@ -28,57 +28,48 @@ object PuzzleSolver {
   @throws[IllegalArgumentException]
   def solve(startBoard: PuzzleBoard): Option[PuzzleBoard] =  {
 
-    //first check that the board is solvable
+    //first check that the board is solvable (step 1)
     require(startBoard.solvable)
 
-    //If the heuristic is 2 move one piece and return the solved board
-    if (startBoard.heuristic == 2) {
-      startBoard.makeMove(14)
-      startBoard.makeMove(15)
-      Some(startBoard)
-    }
-    else {
-      def boardOrder(boardInput: PuzzleBoard) = boardInput.heuristic
+    def boardOrder(boardInput: PuzzleBoard) = boardInput.heuristic
 
-      //create an empty priority queue
-      val boardPriorityQueue: mutable.PriorityQueue[PuzzleBoard] = mutable.PriorityQueue.empty[PuzzleBoard](
-        Ordering.by(boardOrder))
+    //create an empty priority queue (step 2)
+    val boardPriorityQueue: mutable.PriorityQueue[PuzzleBoard] = mutable.PriorityQueue.empty[PuzzleBoard](
+      Ordering.by(boardOrder))
 
-      //enqueue the startBoard to the queue
-      boardPriorityQueue.enqueue(startBoard)
+    //enqueue the startBoard to the queue (step 3)
+    boardPriorityQueue.enqueue(startBoard)
 
-      //use best first search to find the solution
-      while (boardPriorityQueue.nonEmpty) {
+    //use best first search to find the solution (step 4)
+    while (boardPriorityQueue.nonEmpty) {
 
-        //Choose the last board in the queue as it has the lowest heuristic value
-        val tempBoard: PuzzleBoard = boardPriorityQueue.last
+      //Choose the last board in the queue as it has the lowest heuristic value
+      if (boardPriorityQueue.last.heuristic == 0) boardPriorityQueue.last
 
-        //Find all the possible valid moves for that board
-        val validMoves: Seq[Int] = tempBoard.validMoves()
+      //Find all the possible valid moves for that board
+      val validMoves: Seq[Int] = boardPriorityQueue.last.validMoves()
 
-        //If there are no valid moves for that board return None
-        if (validMoves.isEmpty) None
+      //If there are no valid moves for that board return None
+      if (validMoves.isEmpty) None
 
-        else {
+      else {
 
-          //for each possible move create a new board and make move and enqueue it to the queue
-          for (moves <- validMoves) {
-            val tempBoard1: PuzzleBoard = tempBoard
+        //for each possible move create a new board and make move and enqueue it to the queue
+        for (moves <- validMoves) {
+          val tempBoard: PuzzleBoard = startBoard
 
-            //If the heuristic is 0 return the board
-            if (tempBoard1.heuristic == 0) Some(tempBoard1)
+          //If the heuristic is 0 return the board
+          if (tempBoard.heuristic == 0) Some(tempBoard)
 
-            else {
-              boardPriorityQueue.enqueue(tempBoard1.makeMove(moves))
-              boardPriorityQueue.dequeue()
-            }
+          else {
+            boardPriorityQueue.enqueue(tempBoard.makeMove(moves))
+            boardPriorityQueue.dequeue()
           }
         }
       }
-      None
     }
+    None
   }
-
   /*** No changes below this line ***/
 
 }
